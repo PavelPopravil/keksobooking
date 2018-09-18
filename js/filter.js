6b47eea644451d71d3ffacb353c220391d231057
Shishic <pasukmc@gmail.com>
2018-09-18 13:56:07 +0300

    finish task 7

--- js/filter.js
+++ js/filter.js
@@ -6,15 +6,38 @@
     var housePrice = filters.querySelector('#housing_price');
     var houseRoomNum = filters.querySelector('#housing_room-number');
     var houseGuestNum = filters.querySelector('#housing_guests-number');
-    var selectList = filters.querySelectorAll('.tokyo__filter');
+    var features = filters.querySelector('#housing_features');
 
-
+    var Prices = {
+        min: 10000,
+        max: 50000
+    };
 
     var selectData = [
         {
             selector: houseType,
             cb: function (data) {
                 return this.selector.value === 'any' ? true : this.selector.value === data.offer.type;
+            }
+        },
+        {
+            selector: housePrice,
+            cb: function (data) {
+                switch (this.selector.value) {
+                    case 'any':
+                        return true;
+                        break;
+                    case 'low':
+                        return data.offer.price <= Prices.min;
+                        break;
+                    case 'middle':
+                        return Prices.min <= data.offer.price && data.offer.price <= Prices.max;
+                        break;
+                    case 'high':
+                        return data.offer.price >= Prices.max;
+                    default:
+                        return true;
+                }
             }
         },
         {
@@ -28,11 +51,19 @@
             cb: function (data) {
                 return this.selector.value === 'any' ? true : parseInt(this.selector.value) === data.offer.guests;
             }
+        },
+        {
+            selector: features,
+            cb: function (data) {
+                var checkedFeatures = features.querySelectorAll('[type="checkbox"]:checked');
+                return Array.from(checkedFeatures).every(function (feature) {
+                    return data.offer.features.includes(feature.value);
+                })
+            }
         }
     ];
 
     function filterAllFields(offerItem) {
-
         return selectData.every(function (item) {
             return item.cb(offerItem);
         });
@@ -51,8 +82,12 @@
         if (!filters) {
             return false;
         }
+        var timer = null;
         filters.addEventListener('change', function () {
-            debouncedUpdatePins();
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                debouncedUpdatePins();
+            }, 300);
         });
     }
 
